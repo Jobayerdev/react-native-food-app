f282bcb8ad1496bff2b73f764974600152b223b6
Jobayer Hossain <jobayerhossain977@Gmail.com>
2020-11-13 15:39:22 +0600

    Planet UI Init

---
+++ screens/Index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Index = () => {
+    return (
+        <View>
+            <Text>
+                
+            </Text>
+        </View>
+    )
+}
+
+export default Index
+
